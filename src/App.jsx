0dff3def8746ab2623330429d00532620f49f3d6
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-07-14 11:41:14 +0200

    Name added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+
+        <h1>Hasib</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
